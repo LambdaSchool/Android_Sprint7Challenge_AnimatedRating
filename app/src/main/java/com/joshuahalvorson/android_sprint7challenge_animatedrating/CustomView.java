bd5ce9d1274ddf17a33ca18af5a852a00fd38b78
Josh Halvorson <Jhalvorson5@mail.greenriver.edu>
2019-01-18 10:09:25 -0800

    Activity can now get or set the starting rating.

--- app/src/main/java/com/joshuahalvorson/android_sprint7challenge_animatedrating/CustomView.java
+++ app/src/main/java/com/joshuahalvorson/android_sprint7challenge_animatedrating/CustomView.java
@@ -6,10 +6,8 @@ import android.graphics.drawable.Animatable;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,6 +35,15 @@ public class CustomView extends LinearLayout {
     public CustomView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
         init(attrs);
+    }
+
+    public int getStartingRating() {
+        return startingRating;
+    }
+
+    public void setStartingRating(int startingRating) {
+        this.startingRating = startingRating;
+        setAnimatedDrawable(startingRating - 1);
     }
 
     private void setAnimatedDrawable(int index){
--- app/src/main/java/com/joshuahalvorson/android_sprint7challenge_animatedrating/MainActivity.java
+++ app/src/main/java/com/joshuahalvorson/android_sprint7challenge_animatedrating/MainActivity.java
@@ -9,5 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CustomView customView = findViewById(R.id.custom_view);
+        customView.setStartingRating(5);
     }
 }
