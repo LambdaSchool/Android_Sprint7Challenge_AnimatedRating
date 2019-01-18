1c879680048907e699a643808562a07d572e62e0
Josh Halvorson <Jhalvorson5@mail.greenriver.edu>
2019-01-18 09:24:49 -0800

    Added attrs for custom view.

--- app/src/main/java/com/joshuahalvorson/android_sprint7challenge_animatedrating/CustomView.java
+++ app/src/main/java/com/joshuahalvorson/android_sprint7challenge_animatedrating/CustomView.java
@@ -1,8 +1,11 @@
 package com.joshuahalvorson.android_sprint7challenge_animatedrating;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.LinearLayout;
 
 public class CustomView extends LinearLayout {
@@ -27,6 +30,21 @@ public class CustomView extends LinearLayout {
     }
 
     protected void init(AttributeSet attrs){
+        setOrientation(HORIZONTAL);
 
+        TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.CustomView);
+
+        int emptyImage =  typedArray.getResourceId(R.styleable.CustomView_emptyImage, R.drawable.ic_launcher_background);
+        int filledImage =  typedArray.getResourceId(R.styleable.CustomView_filledImage, R.drawable.ic_launcher_foreground);
+        int maxRating = typedArray.getInt(R.styleable.CustomView_maxRating, 5);
+        int startingRating = typedArray.getInt(R.styleable.CustomView_startingRating, 3);
+
+        if(startingRating < maxRating){
+            for(int i = 1; i <= startingRating; i++){
+                CustomImageView imageView = new CustomImageView(getContext());
+                imageView.setImageDrawable(getResources().getDrawable(emptyImage));
+                addView(imageView);
+            }
+        }
     }
 }
