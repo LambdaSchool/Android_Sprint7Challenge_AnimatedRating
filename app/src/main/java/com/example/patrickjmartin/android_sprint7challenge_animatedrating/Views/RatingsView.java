c7379f86241eecbf6b6575d0562f997678bc81d7
Patrick Martin <patrickmartin88@gmail.com>
2019-01-18 08:30:58 -0800

    Added attrs xml to alter ratings view.

---
+++ app/src/main/java/com/example/patrickjmartin/android_sprint7challenge_animatedrating/Views/RatingsView.java
@@ -0,0 +1,31 @@
+package com.example.patrickjmartin.android_sprint7challenge_animatedrating.Views;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+import android.support.annotation.Nullable;
+
+public class RatingsView extends View {
+
+    public RatingsView(Context context) {
+        super(context);
+    }
+
+    public RatingsView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RatingsView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public RatingsView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
