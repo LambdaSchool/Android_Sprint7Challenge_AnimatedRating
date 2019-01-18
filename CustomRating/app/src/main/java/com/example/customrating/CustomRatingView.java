15c052e09491cb6f8e3fd5dbe9109ef34ce21ee2
ThaDocsN <charles.tha.doc@gamil.com>
2019-01-18 12:18:04 -0600

    finished the init method

--- CustomRating/app/src/main/java/com/example/customrating/CustomRatingView.java
+++ CustomRating/app/src/main/java/com/example/customrating/CustomRatingView.java
@@ -1,12 +1,15 @@
 package com.example.customrating;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.support.v7.widget.AppCompatRatingBar;
 import android.util.AttributeSet;
 import android.widget.RatingBar;
 
 public class CustomRatingView extends AppCompatRatingBar {
+    protected int maximum, starting, empty, filled;
+
 
     public CustomRatingView(Context context) {
         super(context);
@@ -18,6 +21,17 @@ public class CustomRatingView extends AppCompatRatingBar {
 
     public CustomRatingView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+    }
+
+    public void init(AttributeSet attrs){
+        if (attrs != null){
+            TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.CustomRatingView);
+            maximum = typedArray.getInt(R.styleable.CustomRatingView_maximum_rating, 5);
+            starting = typedArray.getInt(R.styleable.CustomRatingView_starting_rating, 2);
+            empty = typedArray.getResourceId(R.styleable.CustomRatingView_empty_symbol, R.color.colorAccent);
+            filled = typedArray.getResourceId(R.styleable.CustomRatingView_fill_symbol, R.color.colorPrimary);
+            typedArray.recycle();
+        }
     }
 
     @Override
