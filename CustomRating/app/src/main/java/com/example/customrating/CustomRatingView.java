a4bbf2236177819866083f06a7a9b8d33633dadd
ThaDocsN <charles.tha.doc@gamil.com>
2019-01-18 11:16:40 -0600

    had to change custom view type, was getting an error

--- CustomRating/app/src/main/java/com/example/customrating/CustomRatingView.java
+++ CustomRating/app/src/main/java/com/example/customrating/CustomRatingView.java
@@ -2,33 +2,27 @@ package com.example.customrating;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.support.v7.widget.AppCompatRatingBar;
 import android.util.AttributeSet;
 import android.widget.RatingBar;
 
-public class CustomRatingView extends RatingBar {
-    public CustomRatingView(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-    }
+public class CustomRatingView extends AppCompatRatingBar {
 
-    public CustomRatingView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
+    public CustomRatingView(Context context) {
+        super(context);
     }
 
     public CustomRatingView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
-    public CustomRatingView(Context context) {
-        super(context);
-    }
-
-    protected void init(AttributeSet attrs){
-
-
+    public CustomRatingView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
     }
 
     @Override
     protected synchronized void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        RatingBar ratingBar;
     }
 }
