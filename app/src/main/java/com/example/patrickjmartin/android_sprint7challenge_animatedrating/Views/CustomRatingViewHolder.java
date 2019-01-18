26372c78b1960f0e9d7e815c2a5dd54671bffd57
Patrick Martin <patrickmartin88@gmail.com>
2019-01-18 10:49:41 -0800

    Scrapped vector drawing for ImageView animation instead.

---
+++ app/src/main/java/com/example/patrickjmartin/android_sprint7challenge_animatedrating/Views/CustomRatingViewHolder.java
@@ -0,0 +1,4 @@
+package com.example.patrickjmartin.android_sprint7challenge_animatedrating.Views;
+
+public class CustomRatingViewHolder {
+}
--- app/src/main/java/com/example/patrickjmartin/android_sprint7challenge_animatedrating/Views/RatingsView.java
+++ app/src/main/java/com/example/patrickjmartin/android_sprint7challenge_animatedrating/Views/RatingsView.java
@@ -1,31 +1,67 @@
 package com.example.patrickjmartin.android_sprint7challenge_animatedrating.Views;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.AttributeSet;
 import android.view.View;
 import android.support.annotation.Nullable;
+import android.widget.ImageView;
 
-public class RatingsView extends View {
+import com.example.patrickjmartin.android_sprint7challenge_animatedrating.R;
+
+public class RatingsView extends android.support.v7.widget.AppCompatImageView {
+
+    public boolean isFull = false;
 
     public RatingsView(Context context) {
         super(context);
+        init(null);
     }
 
     public RatingsView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init(attrs);
     }
 
     public RatingsView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(attrs);
     }
 
-    public RatingsView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
+    public boolean isFull(){
+        return isFull;
     }
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
+    public void setFull(boolean full) {
+        isFull = full;
     }
+
+    protected void init(AttributeSet attrs) {
+    }
+
+    
+
+
+
 }
+
+
+//        float mid = getWidth() / 2;
+//        float min = Math.min(getWidth(), getHeight());
+//        float fat = min / 17;
+//        float half = min / 2;
+//
+//        bigPaint.setStrokeWidth(fat);
+//        bigPaint.setStyle(Paint.Style.STROKE);
+//
+//        bigStar.moveTo(mid + half * 0.5f, half * 0.84f);
+//        bigStar.lineTo(mid + half * 1.5f, half * 0.84f);
+//        bigStar.lineTo(mid + half * 0.68f, half * 1.45f);
+//        bigStar.lineTo(mid + half * 1.0f, half * 0.5f);
+//        bigStar.lineTo(mid + half * 1.32f, half * 1.45f);
+//        bigStar.lineTo(mid + half * 0.5f, half * 0.84f);
