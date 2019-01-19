79c2f602c5803e75265ad7016f016c797dc0cc7e
Patrick Martin <patrickmartin88@gmail.com>
2019-01-18 17:30:17 -0800

    Implemented half screen touch listener to change star values.

--- app/src/main/java/com/example/patrickjmartin/android_sprint7challenge_animatedrating/Views/CustomRatingViewHolder.java
+++ app/src/main/java/com/example/patrickjmartin/android_sprint7challenge_animatedrating/Views/CustomRatingViewHolder.java
@@ -1,17 +1,23 @@
 package com.example.patrickjmartin.android_sprint7challenge_animatedrating.Views;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.graphics.BitmapFactory;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.PagerTabStrip;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
-
-import com.example.patrickjmartin.android_sprint7challenge_animatedrating.R;
 
 import java.util.ArrayList;
 
@@ -19,6 +25,9 @@ public class CustomRatingViewHolder extends LinearLayout {
 
     private ArrayList<RatingsView> views;
     private int max_stars, initial_stars, empty_star, full_star;
+    private float x1, x2;
+    int screenHalf;
+
 
     public CustomRatingViewHolder(@NonNull Context context) {
         super(context);
@@ -41,6 +50,9 @@ public class CustomRatingViewHolder extends LinearLayout {
     }
 
     public void init(AttributeSet attrs) {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        ((Activity)getContext()).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        screenHalf = displayMetrics.widthPixels / 2;
 
         views = new ArrayList<>();
 
@@ -62,10 +74,29 @@ public class CustomRatingViewHolder extends LinearLayout {
                 }
                 star.setImageDrawable(getResources().getDrawable(empty_star));
                 addView(star);
-            }
 
+
+            }
 
         }
     }
 
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+
+
+        int touchLocation = (int) ev.getX();
+
+        if(touchLocation < screenHalf) {
+            Toast.makeText(getContext(), "Touched the left side", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getContext(), "Touched the right side", Toast.LENGTH_SHORT).show();
+        }
+
+
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    
 }
