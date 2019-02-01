5c4e24007a4e32edc43982a9885f30aafc403408
Pablo Ogata <pabloogata@gmail.com>
2019-02-01 12:00:42 -0500

    start clickble

--- app/src/main/java/com/example/android_sprint7challenge_animatedrating/MainActivity.java
+++ app/src/main/java/com/example/android_sprint7challenge_animatedrating/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.android_sprint7challenge_animatedrating;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    RatingBar ratingBar;
+    Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+        button = (Button) findViewById(R.id.button);
+
+    ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+        @Override
+        public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+            Toast.makeText(MainActivity.this, "Stars: " + (int)rating, Toast.LENGTH_SHORT).show();
+        }
+    });
+
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Start:" +(int)ratingBar.getRating(), Toast.LENGTH_SHORT).show();
+            }
+        });
+                
+
     }
 }
