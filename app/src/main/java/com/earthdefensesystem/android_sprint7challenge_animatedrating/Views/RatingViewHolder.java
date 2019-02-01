9df14111a488d6f9b3128ef50f6cc7526a04bb66
JTyzz <jtyzzer@gmail.com>
2019-02-01 12:00:51 -0800

    Added star animations and Views.

---
+++ app/src/main/java/com/earthdefensesystem/android_sprint7challenge_animatedrating/Views/RatingViewHolder.java
@@ -0,0 +1,24 @@
+package com.earthdefensesystem.android_sprint7challenge_animatedrating.Views;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class RatingViewHolder extends View {
+    public RatingViewHolder(Context context) {
+        super(context);
+    }
+
+    public RatingViewHolder(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RatingViewHolder(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public RatingViewHolder(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
--- app/src/main/java/com/earthdefensesystem/android_sprint7challenge_animatedrating/Views/StarRatingView.java
+++ app/src/main/java/com/earthdefensesystem/android_sprint7challenge_animatedrating/Views/StarRatingView.java
@@ -5,20 +5,35 @@ import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
-public class StarRatingView extends View {
+public class StarRatingView extends android.support.v7.widget.AppCompatImageView {
+
+    public boolean isFull = false;
+
     public StarRatingView(Context context) {
         super(context);
+        init(null);
     }
 
     public StarRatingView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init(attrs);
     }
 
     public StarRatingView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(attrs);
     }
 
-    public StarRatingView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
+
+    public boolean isFull(){
+        return isFull;
+    }
+
+    public void setFull(boolean full){
+        isFull = full;
+    }
+
+    protected void init(AttributeSet attrs){
+
     }
 }
