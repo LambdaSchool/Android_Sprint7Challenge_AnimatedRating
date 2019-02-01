560a6f110191063de618980bf0f56a856c9d2d89
JTyzz <jtyzzer@gmail.com>
2019-02-01 08:28:41 -0800

    Initial commit

---
+++ app/src/main/java/com/earthdefensesystem/android_sprint7challenge_animatedrating/Views/StarRatingView.java
@@ -0,0 +1,24 @@
+package com.earthdefensesystem.android_sprint7challenge_animatedrating.Views;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class StarRatingView extends View {
+    public StarRatingView(Context context) {
+        super(context);
+    }
+
+    public StarRatingView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public StarRatingView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public StarRatingView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
