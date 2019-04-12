b69c0f99160166a6313c78f9596d9f932c93fa3b
Israel dela Cruz <igdcruz42@gmail.com>
2019-04-12 10:39:51 -0500

    can preview hard coded rating on the layout editor

--- Sprint4/app/src/main/java/com/example/israel/sprint4/RatingView.java
+++ Sprint4/app/src/main/java/com/example/israel/sprint4/RatingView.java
@@ -1,12 +1,16 @@
 package com.example.israel.sprint4;
 
 import android.content.Context;
-import android.support.annotation.Nullable;
+import android.content.res.TypedArray;
 import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
-import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
-public class RatingView extends ViewGroup {
+import java.util.ArrayList;
+
+public class RatingView extends LinearLayout {
+
+    public static final int MIN_RATING = 1;
 
     public RatingView(Context context) {
         super(context);
@@ -21,15 +25,61 @@ public class RatingView extends ViewGroup {
     }
 
     private void init(AttributeSet attrs) {
-        if (attrs != null) {
+        setOrientation(LinearLayout.HORIZONTAL);
 
+        if (attrs != null) {
+            TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.RatingView);
+            maxRating = typedArray.getInteger(R.styleable.RatingView_maxRating, MIN_RATING);
+            rating = typedArray.getInteger(R.styleable.RatingView_rating, MIN_RATING);
+            // clamp
+            if (rating < MIN_RATING) {
+                rating = MIN_RATING;
+            } else if (rating > maxRating) {
+                rating = maxRating;
+            }
+
+            emptySymbol = R.drawable.avd_star_filled_to_empty;//typedArray.getInteger(R.styleable.RatingView_emptySymbol, 0);
+            filledSymbol = R.drawable.avd_star_empty_to_filled;//typedArray.getInteger(R.styleable.RatingView_filledSymbol, 0);
+        } else {
+            maxRating = MIN_RATING;
+            rating = MIN_RATING;
         }
 
-
+        for (int i = 0; i < maxRating; ++i) {
+            SymbolView symbolView = new SymbolView(getContext());
+            int imageResId = rating > i ? filledSymbol : emptySymbol;
+            symbolView.setImageDrawable(getContext().getDrawable(imageResId));
+            addView(symbolView);
+            symbolViews.add(symbolView);
+        }
     }
 
-    @Override
-    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+    private int maxRating;
+    private int rating;
+    private int emptySymbol;
+    private int filledSymbol;
+    private ArrayList<SymbolView> symbolViews = new ArrayList<>();
+
+//    @Override
+//    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+//
+//    }
+
+    public void setMaxRating(int maxRating) {
+        if (maxRating == this.maxRating || maxRating < MIN_RATING) {
+            return;
+        }
+
+        if (maxRating < this.maxRating) { // decrease symbol
+
+
+
+        } else { // increase symbol
+
+        }
+    }
+
+    public void setRating(int rating) {
 
     }
 
@@ -37,10 +87,7 @@ public class RatingView extends ViewGroup {
 
         public SymbolView(Context context) {
             super(context);
-        }
 
-        public SymbolView(Context context, @Nullable AttributeSet attrs) {
-            super(context, attrs);
         }
 
 
