61ebcfa43f79dbc01148ae92fa4af9fe71537aef
Kyle Cole <kyle@misfitselectronics.com>
2019-04-12 13:36:22 -0400

    Project finished, Implemented my own stretch goal to implement a toast with the rating (was a bit hard to actually implement), and changed up the color scheme a bit for better asthetics.

--- app/src/main/java/com/example/android_sprint7challenge_animatedrating/MainActivity.java
+++ app/src/main/java/com/example/android_sprint7challenge_animatedrating/MainActivity.java
@@ -9,5 +9,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //2.i-2.ii mvp goal, showing that you can programatically set number of views (max rating) and initial rating(initialRating)
+        //3 mvp goal allows activity code to set or get rating
+        CustomView customView = findViewById(R.id.custom_view);
+        customView.setMaxRating(5);
+        customView.setInitialRating(4);
     }
 }
