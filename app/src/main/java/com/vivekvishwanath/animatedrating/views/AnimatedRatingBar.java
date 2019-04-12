f68c20a2659791e995c6140350734c256f0715ac
VivekV95 <vivekv210@gmail.com>
2019-04-12 10:50:11 -0500

    Added ability to display rating view with custom ratings and images

--- app/src/main/java/com/vivekvishwanath/animatedrating/views/AnimatedRatingBar.java
+++ app/src/main/java/com/vivekvishwanath/animatedrating/views/AnimatedRatingBar.java
@@ -1,9 +1,12 @@
 package com.vivekvishwanath.animatedrating.views;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.widget.LinearLayout;
+
+import com.vivekvishwanath.animatedrating.R;
 
 import java.util.ArrayList;
 
@@ -12,20 +15,52 @@ public class AnimatedRatingBar extends LinearLayout {
     private int maxRating, startingRating;
     private int emptySymbolId, filledSymbolId;
     private ArrayList<RatingSymbol> ratingSymbols;
+    private final int DEFAULT_MAX_RATING = 10;
+    private final int DEFAULT_STARTING_RATING = 5;
 
     public AnimatedRatingBar(Context context) {
         super(context);
+        init(null);
     }
 
     public AnimatedRatingBar(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init(attrs);
     }
 
     public AnimatedRatingBar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(attrs);
     }
 
     public AnimatedRatingBar(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        init(attrs);
+    }
+
+    public void init(AttributeSet attrs) {
+        if (attrs != null) {
+            TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.AnimatedRatingBar);
+            maxRating = typedArray.getInt(R.styleable.AnimatedRatingBar_max_rating, DEFAULT_MAX_RATING);
+            startingRating = typedArray.getInt(R.styleable.AnimatedRatingBar_starting_rating, DEFAULT_STARTING_RATING);
+            emptySymbolId = typedArray.getResourceId(R.styleable.AnimatedRatingBar_empty_symbol, R.drawable.ic_launcher_foreground);
+            filledSymbolId = typedArray.getResourceId(R.styleable.AnimatedRatingBar_filled_symbol, R.drawable.ic_launcher_background);
+        }
+
+        ratingSymbols = new ArrayList<>();
+
+        for (int i = 1; i <= maxRating; i++) {
+            final RatingSymbol ratingSymbol = new RatingSymbol(getContext());
+            if (i <= startingRating) {
+                ratingSymbol.setFilled(true);
+                ratingSymbol.setImageResource(filledSymbolId);
+            } else {
+                ratingSymbol.setFilled(false);
+                ratingSymbol.setImageResource(emptySymbolId);
+            }
+            ratingSymbols.add(ratingSymbol);
+            addView(ratingSymbol);
+            ratingSymbol.getLayoutParams().width = 50;
+        }
     }
 }
