d09a2f51a39268ad430fe94ed2104046ed6f161d
Jake Esveld <thejakeesveld@gmail.com>
2019-04-12 08:12:28 -0700

    Add empty custom view and attrs for it

---
+++ app/src/main/java/com/jakeesveld/sprint4_challenge/RatingsView.java
@@ -0,0 +1,28 @@
+package com.jakeesveld.sprint4_challenge;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class RatingsView extends LinearLayout {
+    public RatingsView(Context context) {
+        super(context);
+    }
+
+    public RatingsView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RatingsView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public RatingsView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    public void init(){
+
+    }
+}
