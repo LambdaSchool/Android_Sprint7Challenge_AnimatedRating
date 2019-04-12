caf999800064771bdf8da6669eb059c00d424560
BL-CoderOvertime <brandonlively@live.com>
2019-04-12 14:29:28 -0400

    Added TouchEvent hold and swipe to add hearts.

--- app/src/main/java/com/example/androidsprint4/HeartLifeGauge.java
+++ app/src/main/java/com/example/androidsprint4/HeartLifeGauge.java
@@ -2,84 +2,176 @@ package com.example.androidsprint4;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.view.Display;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class HeartLifeGauge extends LinearLayout {
-    int sizeOfHearts, displayWidth;
+    int displayWidth, currentHealth, maxHearts, startingHearts,counter,heartSize;
+    float X,diffX;
     ImageView[] heartArr;
+    Drawable emptyToFullDrawable;
+    Drawable fullToEmptyDrawable;
+
     public HeartLifeGauge(Context context) {
         super(context);
-        init();
+        init(null);
     }
 
     public HeartLifeGauge(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        init();
+        init(attrs);
     }
 
     public HeartLifeGauge(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init();
+        init(attrs);
     }
 
     public HeartLifeGauge(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
-        init();
+        init(attrs);
     }
 
-    private void init(){
+    public void setMaxHearts(int maxHearts) {
+        this.maxHearts = maxHearts;
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                X = event.getX();
+                invalidate();
+                counter = 0;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                diffX = event.getX() - X;
+                counter = (int) (diffX/heartSize);
+                if((X-event.getX() > 0)&&counter>1){
+                    invalidate();
+                }
+                if((X-event.getX() < 0)&&(heartSize/Math.abs(diffX))>1){
+                    invalidate();
+                }
+                invalidate();
+                break;
+            case MotionEvent.ACTION_UP:
+                counter = Math.abs(counter);
+                if(diffX>0){
+                    for(int i = 0; i < counter; i++){
+                        addHeart();
+                        Toast.makeText(getContext(), "Added : " + String.valueOf(counter) + " Hearts", Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    for(int i = 0; i < counter; i++){
+                        reduceHeart();
+                        Toast.makeText(getContext(), "Reduced : " + String.valueOf(counter) + " Hearts", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                invalidate();
+                break;
+
+        }
+
+        return true;
+    }
+
+    private void init(AttributeSet attrs) {
         DisplayMetrics displayMetrics = new DisplayMetrics();
         ((Activity) getContext()).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-        displayWidth  = displayMetrics.widthPixels;
-        int numOfMaxHearts = 10;
-        int sizeOfStartingFullHearts = 3;
-        sizeOfHearts = displayWidth/numOfMaxHearts;
-        heartArr = new ImageView[numOfMaxHearts];
+
+        maxHearts = 10;
+        startingHearts = 3;
+
+        if (attrs != null) {
+            TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.HeartLifeGauge);
+            startingHearts = typedArray.getInt(R.styleable.HeartLifeGauge_starting_health, 3);
+            maxHearts = typedArray.getInt(R.styleable.HeartLifeGauge_max_health, 10);
+        }
+
+        displayWidth = displayMetrics.widthPixels;
+        fullToEmptyDrawable = getResources().getDrawable(R.drawable.avd_heart_full_to_empty);
+        emptyToFullDrawable = getResources().getDrawable(R.drawable.avd_heart_empty_to_full);
+        currentHealth = startingHearts;
+        heartSize = displayWidth / maxHearts;
+        heartArr = new ImageView[maxHearts];
+
+
         boolean isStartingHeartsSet = false;
 
-        for(int i = 0; i < heartArr.length; ++i){
-            if(!isStartingHeartsSet){
-            for(int j = 0; j < sizeOfStartingFullHearts-1; j++)
-            {
-                heartArr[i] = new ImageView(getContext());
-                heartArr[i].setImageResource(R.drawable.ic_full_heart_container);
-                heartArr[i].requestLayout();
-                heartArr[i].setLayoutParams(new LayoutParams(sizeOfHearts,sizeOfHearts));
-                this.addView(heartArr[i]);
-                i++;
+        for (int i = 0; i < heartArr.length; ++i) {
+            if (!isStartingHeartsSet) {
+                for (int j = 0; j < startingHearts; j++) {
+                    heartArr[i] = new ImageView(getContext());
+                    heartArr[i].setImageResource(R.drawable.ic_full_heart_container);
+                    // heartArr[i].requestLayout();
+                    heartArr[i].setLayoutParams(new LayoutParams(heartSize, heartSize));
+                    this.addView(heartArr[i]);
+                    i++;
+                }
+                isStartingHeartsSet = true;
             }
-            isStartingHeartsSet=true;}
             heartArr[i] = new ImageView(getContext());
             heartArr[i].setImageResource(R.drawable.ic_empty_heart_container);
-            heartArr[i].requestLayout();
-            heartArr[i].setLayoutParams(new LayoutParams(sizeOfHearts,sizeOfHearts));
+            // heartArr[i].requestLayout();
+            heartArr[i].setLayoutParams(new LayoutParams(heartSize, heartSize));
             this.addView(heartArr[i]);
         }
     }
 
-    public static void emptyToFull(int position){
-
+    public int getCurrentHealth() {
+        return currentHealth;
     }
 
-    public static void fullToEmpty(int position){
-
+    public void addHeart() {
+        if (currentHealth < maxHearts) {
+            heartArr[currentHealth].setImageDrawable(emptyToFullDrawable);
+            if (heartArr[currentHealth].getDrawable() instanceof Animatable) {
+                ((Animatable) heartArr[currentHealth].getDrawable()).start();
+            }
+            currentHealth++;
+            invalidate();
+        }
     }
 
-    public static void updateViews(){
+    public void reduceHeart() {
+        if (currentHealth > 0) {
+            heartArr[currentHealth - 1].setImageDrawable(fullToEmptyDrawable);
+            if (heartArr[currentHealth - 1].getDrawable() instanceof Animatable) {
+                Animatable animatable = (Animatable) heartArr[currentHealth - 1].getDrawable();
+                animatable.start();
+            }
+            currentHealth--;
+            invalidate();
+        }
+    }
+
+    public void updateViews() {
 
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
+        displayWidth = this.getWidth() / maxHearts;
+        for (int i = 0; i < heartArr.length; ++i) {
+            heartArr[i].requestLayout();
+            heartArr[i].setLayoutParams(new LayoutParams(heartSize, heartSize));
+        }
 
         super.onDraw(canvas);
     }
--- app/src/main/java/com/example/androidsprint4/MainActivity.java
+++ app/src/main/java/com/example/androidsprint4/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.androidsprint4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    HeartLifeGauge hlg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        hlg = findViewById(R.id.heart_gauge);
+
+        Button addHeart = findViewById(R.id.button_increase);
+        Button subHeart = findViewById(R.id.button_decrease);
+
+        addHeart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hlg.addHeart();
+            }
+        });
+
+        subHeart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hlg.reduceHeart();
+            }
+        });
+
     }
 }
