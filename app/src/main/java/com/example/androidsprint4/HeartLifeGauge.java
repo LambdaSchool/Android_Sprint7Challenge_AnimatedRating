3e03fd0fc1539474fc8719434a2a74331d44885c
BL-CoderOvertime <brandonlively@live.com>
2019-04-12 11:50:07 -0400

    created heartLifeGauge customView and setup attrs file

---
+++ app/src/main/java/com/example/androidsprint4/HeartLifeGauge.java
@@ -0,0 +1,24 @@
+package com.example.androidsprint4;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class HeartLifeGauge extends View {
+    public HeartLifeGauge(Context context) {
+        super(context);
+    }
+
+    public HeartLifeGauge(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public HeartLifeGauge(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public HeartLifeGauge(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
