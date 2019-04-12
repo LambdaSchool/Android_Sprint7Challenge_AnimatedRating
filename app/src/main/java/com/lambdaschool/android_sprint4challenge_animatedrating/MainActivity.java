92fda93857bb1764917630108fdae8b2baddea1c
heiligbasil <basilief@yahoo.com>
2019-04-12 08:26:20 -0700

    Basil Havens - Android_Sprint4_Challenge_AnimatedRating

---
+++ app/src/main/java/com/lambdaschool/android_sprint4challenge_animatedrating/MainActivity.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.android_sprint4challenge_animatedrating;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
