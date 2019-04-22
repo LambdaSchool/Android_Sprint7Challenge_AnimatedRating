d1ba34b6e8cce9e8d3b644acef44df7e42c6e399
Shoonsk8 <shoon@att.net>
2019-04-21 23:09:24 -0400

    Add click lisnter

--- app/src/main/java/com/example/android_sprint7challenge_animatedrating/MainActivity.java
+++ app/src/main/java/com/example/android_sprint7challenge_animatedrating/MainActivity.java
@@ -68,6 +68,22 @@ public class MainActivity extends AppCompatActivity implements Animatable {
 
             }
         });
+        findViewById(R.id.slider_rate).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText eta=findViewById(R.id.input_max);
+                EditText etb=findViewById(R.id.input_starting);
+                EditText etc=findViewById(R.id.input_filled);
+                EditText etd=findViewById(R.id.input_empty);
+                String str=eta.getText().toString();
+                int i=Integer.parseInt(str);
+                sl.setMaxRating(i);
+                sl.setStartingRating(Integer.parseInt(etb.getText().toString()));
+                sl.setStrEmpty(etd.getText().toString());
+                sl.setStrFilled(etc.getText().toString());
+
+            }
+        });
 
 
 
