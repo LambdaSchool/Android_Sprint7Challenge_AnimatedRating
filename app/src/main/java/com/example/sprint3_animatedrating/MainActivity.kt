55c591ecd683b0ea053ae0b8fb2d6f00d4dc21fc
jDanielDetes <jdanieldetes@gmail.com>
2019-08-23 12:27:02 -0400

    45% done with popup window

--- app/src/main/java/com/example/sprint3_animatedrating/MainActivity.kt
+++ app/src/main/java/com/example/sprint3_animatedrating/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.example.sprint3_animatedrating
 
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.transition.Slide
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import android.widget.PopupWindow
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,6 +27,25 @@ class MainActivity : AppCompatActivity() {
         transaction.replace(R.id.frag_holder,fragment)
         transaction.addToBackStack(null)
         transaction.commit()
+
+
+        btn_addRating.setOnClickListener{
+
+            val inflater:LayoutInflater =getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            val view = inflater.inflate(R.layout.fragmentpopup,null)
+
+
+            val popup = PopupWindow(
+                view, LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                popup.elevation = 10.0f
+            }
+
+            
+
+        }
 
 
 
