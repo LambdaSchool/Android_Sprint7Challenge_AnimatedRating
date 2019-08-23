6af56111b3ece076e2ad94e070e363e27bfcdec6
Albert Yakubov <mrayakubov@hotmail.com>
2019-08-23 11:50:23 -0600

    Rating1
    Add Vector Drawable

--- app/src/main/java/com/example/rating1/MainActivity.kt
+++ app/src/main/java/com/example/rating1/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.rating1
 
+import android.graphics.drawable.Animatable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -12,13 +13,10 @@ import androidx.core.app.ComponentActivity.ExtraData
 import androidx.core.content.ContextCompat.getSystemService
 import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import android.widget.Toast
 
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import android.view.View
+import androidx.core.content.ContextCompat
 
 
 class MainActivity : AppCompatActivity() {
@@ -27,10 +25,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        AnimateVectorFun()
+
+
+
+
+
         ratingBar.setOnRatingBarChangeListener(RatingBar.OnRatingBarChangeListener { ratingBar, v, b ->
             tvRatingScale.setText(v.toString())
             when (ratingBar.rating.toInt()) {
                 1 -> tvRatingScale.setText("Very bad")
+
                 2 -> tvRatingScale.setText("Need some improvement")
                 3 -> tvRatingScale.setText("Good")
                 4 -> tvRatingScale.setText("Great")
@@ -42,19 +47,11 @@ class MainActivity : AppCompatActivity() {
         btnSubmit.setOnClickListener(object : View.OnClickListener {
             override fun onClick(view: View) {
                 if (tvRatingScale.getText().toString().isEmpty()) {
-                    Toast.makeText(
-                        this@MainActivity,
-                        "Please fill in feedback text box",
-                        Toast.LENGTH_LONG
-                    ).show()
+                    Toast.makeText(this@MainActivity, "Please fill in feedback text box", Toast.LENGTH_LONG).show()
                 } else {
                     tvRatingScale.setText("")
                     ratingBar.setRating(0f)
-                    Toast.makeText(
-                        this@MainActivity,
-                        "Thank you for sharing your feedback",
-                        Toast.LENGTH_SHORT
-                    ).show()
+                    Toast.makeText(this@MainActivity, "Thank you for sharing your feedback", Toast.LENGTH_SHORT).show()
                 }
             }
         })
@@ -62,6 +59,12 @@ class MainActivity : AppCompatActivity() {
 
 
 
+    }
+
+    private fun AnimateVectorFun() {
+        val animatedVectorDrawable = ContextCompat.getDrawable(this, R.drawable.avd_cust)
+        animation_view.setImageDrawable(animatedVectorDrawable)
+        (animatedVectorDrawable as Animatable).start()
     }
 
 }
