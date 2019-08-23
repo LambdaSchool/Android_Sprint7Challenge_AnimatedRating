c7db3604025c4d571fec53363d5d5d5fdeae92e2
alihaider334 <ali334.haider@gmail.com>
2019-08-23 14:52:26 -0400

    Added Dialogue Fragment and CustomRatingBar

--- app/src/main/java/com/example/customratingbar/MainActivity.kt
+++ app/src/main/java/com/example/customratingbar/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.customratingbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.transition.Explode
+import androidx.fragment.app.DialogFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_add_rating.setOnClickListener {
+            val ratingFragment:DialogFragment = RatingFragment()
+            ratingFragment.show(supportFragmentManager, "Rating Fragment")
+
+        }
     }
 }
--- app/src/main/java/com/example/customratingbar/RatingBarView.kt
+++ app/src/main/java/com/example/customratingbar/RatingBarView.kt
@@ -1,4 +1,34 @@
 package com.example.customratingbar
 
-class RatingBarView {
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.EditText
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+
+class RatingBarView (context: Context, attrs: AttributeSet?): LinearLayout(context, attrs) {
+
+    constructor(context: Context): this(context, null)
+
+    var rating:Int=1
+
+    init {
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.RatingBarView)
+        val textWeight = typedArray.getFloat(R.styleable.RatingBarView_layout_weight, 8f)
+        val textSize = typedArray.getDimension(R.styleable.RatingBarView_textSize, 12f)
+        typedArray.recycle()
+
+        val textLayoutParams = LayoutParams(0, LayoutParams.WRAP_CONTENT, textWeight)
+        val ratingBarLayout = LayoutInflater.from(context).inflate(R.layout.rating_bar_layout, this, false) as LinearLayout
+
+       orientation =  VERTICAL
+        addView(ratingBarLayout)
+
+    }
+
+
+
 }
