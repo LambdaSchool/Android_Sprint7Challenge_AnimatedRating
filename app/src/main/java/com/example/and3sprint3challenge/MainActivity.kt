45a9e31014aa20d5aab90ac013cbd4f1df350ea5
TylerChristian711 <tylerc71197@gmail.com>
2019-08-23 14:17:44 -0400

    add fragment and vector drawable animation

---
+++ app/src/main/java/com/example/and3sprint3challenge/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.and3sprint3challenge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+           button_add.setOnClickListener {
+
+           }
+    }
+}
--- app/src/main/java/com/example/and3sprint3challenge/Starfragment.kt
+++ app/src/main/java/com/example/and3sprint3challenge/Starfragment.kt
@@ -100,3 +100,4 @@ class Starfragment : Fragment() {
             }
     }
 }
+//commit
