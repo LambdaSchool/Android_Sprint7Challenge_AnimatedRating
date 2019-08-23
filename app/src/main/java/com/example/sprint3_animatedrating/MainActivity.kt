eeaf074597c9ebad2cf1f18bc97060f0c684aaf5
jDanielDetes <jdanieldetes@gmail.com>
2019-08-23 12:46:56 -0400

    popup 90% done

--- app/src/main/java/com/example/sprint3_animatedrating/MainActivity.kt
+++ app/src/main/java/com/example/sprint3_animatedrating/MainActivity.kt
@@ -5,9 +5,10 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.transition.Slide
+import android.transition.TransitionManager
+import android.view.Gravity
 import android.view.LayoutInflater
-import android.widget.LinearLayout
-import android.widget.PopupWindow
+import android.widget.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -43,7 +44,31 @@ class MainActivity : AppCompatActivity() {
                 popup.elevation = 10.0f
             }
 
-            
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+                val slideIn = Slide()
+                slideIn.slideEdge = Gravity.TOP
+                popup.enterTransition = slideIn
+
+                val slideout = Slide()
+                slideout.slideEdge = Gravity.RIGHT
+                popup.exitTransition = slideout
+
+            }
+
+            val submit_btn = view.findViewById<Button>(R.id.submit_rating)
+
+            submit_btn.setOnClickListener {
+                popup.dismiss()
+            }
+            popup.setOnDismissListener {
+                Toast.makeText(applicationContext,"Rating Submitted", Toast.LENGTH_SHORT).show()
+            }
+
+            TransitionManager.beginDelayedTransition(main_layout)
+            popup.showAtLocation(
+                main_layout, Gravity.CENTER, 0,0
+            )
 
         }
 
